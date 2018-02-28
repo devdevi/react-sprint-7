6b0fe4b876e8917989d4264f37c8cec2b294d044
visidevi <visidevi@gmail.com>
2018-02-28 17:03:39 -0300

    props ++

--- my-app/src/components/Location.js
+++ my-app/src/components/Location.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
-const Location = () => (
-	<div>
-	 <h1>Santiago</h1>
-	</div>
-	)
+const Location = (props) => {
+	// <div>
+	//  <h1>Santiago</h1>
+	// </div>
+	// const city = props.city;  forma larga 
+	// console.log(props)
+	// console.log(city)
+
+	const {city} = props;
+	debugger; //para ver lo que pasa con props
+	return (
+		<div>
+			<h1>{city}</h1>
+		</div>
+
+		)	
+
+	}
 
 export default Location;
--- my-app/src/components/WeatherLocation.js
+++ my-app/src/components/WeatherLocation.js
@@ -5,7 +5,7 @@ import WeatherData from './WeatherData';
 
 const WeatherLocation = () => (
 	<div>
-		<Location />
+		<Location city ={'MIAMI'}/>
        <WeatherData />
 
 	</div>)
