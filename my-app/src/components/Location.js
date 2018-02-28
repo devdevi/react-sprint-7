fc8bbae62a7ad20993e42eaa46095ed9c6f2f854
visidevi <visidevi@gmail.com>
2018-02-28 17:21:20 -0300

    codigo en una linea

--- my-app/src/components/Location.js
+++ my-app/src/components/Location.js
@@ -1,22 +1,13 @@
 import React from 'react';
 
-const Location = (props) => {
-	// <div>
-	//  <h1>Santiago</h1>
-	// </div>
-	// const city = props.city;  forma larga 
-	// console.log(props)
-	// console.log(city)
+const Location = ({city}) => (
 
-	const {city} = props;
-	debugger; //para ver lo que pasa con props
-	return (
 		<div>
 			<h1>{city}</h1>
 		</div>
 
 		)	
 
-	}
+	
 
 export default Location;
