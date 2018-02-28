556e20ef7c9f985ae185568b1b079c0e65496e2c
visidevi <visidevi@gmail.com>
2018-02-28 15:51:04 -0300

    myfirst commit

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import WeatherLocation from './components/WeatherLocation';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <WeatherLocation></WeatherLocation>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ my-app/src/components/WeatherLocation.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const WeatherLocation = () => (
+	<div>
+			<p>Azullll comomesl mar azul</p>
+
+
+	</div>)
+
+export default WeatherLocation;
