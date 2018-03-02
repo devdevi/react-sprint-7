a93b52098fbcf81030393605929541d3e3a4e1f4
visidevi <visidevi@gmail.com>
2018-03-02 16:44:02 -0300

    api  data

--- my-app/src/components/WeatherLocation/index.js
+++ my-app/src/components/WeatherLocation/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
 
@@ -10,20 +10,56 @@ import {
 		SNOW,
 		WINDY,
 } from './../../constant/weathers';
-const data = {
+
+const data1 = {
 	temperature:20,
 	weatherState: SUN,
 	humidity: 10,
 	wind: 10,
 }
+const data = {
+	temperature:30,
+	weatherState: WINDY,
+	humidity: 80,
+	wind: 200,
+}
+class WeatherLocation extends Component{
+	constructor (){
+		super();
+		this.state = { 
+			city: 'Santiago',
+			data: data
+		}
 
+	}
 
-const WeatherLocation = () => (
-	<div className='weaterDataCont'>
-		<Location city = {'MIAMI'}/>
-       <WeatherData data ={data} />
-
+	handleUpdateClick = () => {
+		console.log('hello visaka')
+		this.setState ({
+			data: data1,
+		})
+	}
+	render = () => {
+		const {city, data} = this.state;
+    return (
+     <div className='weaterDataCont'>
+		<Location city = {city}/>
+       <WeatherData data = {data} />
+       <button onClick = {this.handleUpdateClick}>Actualizar</button>
 	</div>
-	)
+    );
+  }
+
+}
+
+
+
+// const WeatherLocation = () => (
+// 	<div className='weaterDataCont'>
+// 		<Location city = {'MIAMI'}/>
+//        <WeatherData data ={data} />
+
+// 	</div>
+// 	)
 
 export default WeatherLocation;
