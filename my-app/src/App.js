a24a49dde8c2c91d7c2daa2dc3c005813c5e6183
visidevi <visidevi@gmail.com>
2018-03-12 18:22:56 -0300

    redux

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,19 @@
 import React, {Component} from 'react';
-import {connect} from 'react-redux';
+/*import {connect} from 'react-redux'; lo enviamos al container de LOcationLIstCOntainer*/
+import PropTypes from 'prop-types';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 // import {createStore } from 'redux';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Paper from 'material-ui/Paper';
 import AppBar from 'material-ui/AppBar';
-import LocationList from './components/LocationsList';
-import ForecastExtended from './components/ForecastExtendend';
-import {setCity} from './actions';
+/*import LocationList from './components/LocationsList'; lo enviamos al container de LOcationLIstCOntainer */
+/*import ForecastExtended from './components/ForecastExtendend';*/
+// import {setCity} from './actions'; ya no lo usamos aqui 
+import ForecastExtendedContainer from './containers/ForecastExtendendContainer';
+import LocationsListContainer from './containers/LocationsListContainer';
+
 // import {store} from './store';
 import './App.css';
-
 
 const cities = [
 'Santiago,scl',
@@ -21,6 +24,41 @@ const cities = [
 'Rio de Janeiro,br',
 'London,uk'
 ];
+
+
+class App extends Component {
+
+  render() {
+    return (
+		<MuiThemeProvider>
+		<Grid>
+			<Row>
+			<Col xs={12}>
+			<AppBar title='Weather-Location'/>
+			</Col>
+			</Row>
+			<Row>
+				<Col xs={12} md={6}>
+				<LocationsListContainer cities = {cities}/>
+				</Col>
+				<Col xs={12} md={6}>
+				<Paper zDepth = {4}>
+				<div className="locationList">
+					<ForecastExtendedContainer >
+					</ForecastExtendedContainer>
+				</div>
+				</Paper>
+				</Col>
+			</Row>
+			</Grid>
+		</MuiThemeProvider>
+    );
+  }
+}
+
+export  default App;
+
+
 //Redux llevamos las funciones y metodos a las carpetas actions y store 
 //hacer una constate store de reduce con un reducer dentro, 
 // const store = createStore(() => {})
@@ -35,70 +73,92 @@ const cities = [
 })*/
 
 
-class App extends Component {
+// class App extends Component {
 
-	constructor(){
-		super()
-		this.state = {
-			city: null,
-		}
-	}
+// /*	constructor(){
+// 		super()
+// 		this.state = {
+// 			city: null,
+// 		}
+// 	}*/
 
-	 handlerSelectionClick = (city) =>{
-	 	this.setState({city})
-		console.log('handlerSelectionClick', city)
-
-		// const action = {type:'setState', value:city} Las acction no tienen quje estar dentro del componenete
-		// store.dispatch(setCity(city)) yo ya no estoy importando store
-		this.props.setCity(city);
-	}
-  render() {
-
-  	const {city } = this.state;
-    return (
-		<MuiThemeProvider>
-		<Grid>
-			<Row>
-			<Col xs={12}>
-			<AppBar title='Weather-Location'/>
-			</Col>
-			</Row>
-			<Row>
-				<Col xs={12} md={6}>
-				<LocationList cities = {cities}
-				onSelectedLocation = {this.handlerSelectionClick} />
-				</Col>
-				<Col xs={12} md={6}>
-				<Paper zDepth = {4}>
-				<div className="locationList">
-				{
-					city === null ? null : 
-					<ForecastExtended city = {city}> </ForecastExtended>
-				}
-				</div>
-				</Paper>
-				</Col>
-			</Row>
-			</Grid>
+//   render() {
+//   	/*const {city } = this.state;*/
+//     return (
+// 		<MuiThemeProvider>
+// 		<Grid>
+// 			<Row>
+// 			<Col xs={12}>
+// 			<AppBar title='Weather-Location'/>
+// 			</Col>
+// 			</Row>
+// 			<Row>
+// 				<Col xs={12} md={6}>
+// 				<LocationsListContainer cities = {cities}/>
+// 				</Col>
+// 				<Col xs={12} md={6}>
+// 				<Paper zDepth = {4}>
+// 				<div className="locationList">
+// 					<ForecastExtendedContainer>
+// 					</ForecastExtendedContainer>
+// 				</div>
+// 				</Paper>
+// 				</Col>
+// 			</Row>
+// 			</Grid>
 			
 
-		</MuiThemeProvider>
-    );
-  }
-}
-// const mapDispatchPropsActions = () => {};
-//reduz esta esperando parametros si o si toca hacerlo
-/*const componentConnected = connect(null, mapDispatchPropsActions)(App) 
-*/
-const mapDispatchPropsActions = dispatch => ({
+// 		</MuiThemeProvider>
+//     );
+//   }
+// }
+// // const mapDispatchPropsActions = () => {};
+// //reduz esta esperando parametros si o si toca hacerlo
+// /*const componentConnected = connect(null, mapDispatchPropsActions)(App) 
+// */
 
-	setCity: value => dispatch(setCity(value))
-});
-
-const AppConnected = connect(null, mapDispatchPropsActions)(App) 
-
-
-export  default AppConnected;
+// 	{
+// 				city === null ? null : 
+// 					<ForecastExtendedContainer city = {city}/>
+// 				}
 
 
+// 				(
+// 				this.props.city && 
+// 				<ForecastExtended city = {this.props.city} />
+// 		)
 
+// //Valodamos app, antes de cambiar el nombre;
+
+// /*
+// App.propTypes ={
+// 	setCity:PropTypes.func.isRequired, Validamos pero ya no lo usamos }
+// */	
+
+// /*dispatch: SOlicita el cambio al estado, 
+//  permite que el estado sea actualizado
+//   a través del un action o función, que 
+//   tiene una llave type, y los valores que 
+//   quisiéramos  agregar*/
+// /*const mapDispatchPropsActions = dispatch => ({
+
+// 	setCity: value => dispatch(setCity(value))
+// }); lo enviamos al container de LOcationLIstCOntainer */
+// //duvuelve un objeto y le pasa el estado a redux 
+
+
+// /* Esto lo haciamos antes asi LUnes !!!
+// const AppConnected = connect(null, mapDispatchPropsActions)(App) 
+
+
+
+// // conecta react con reduce
+// export  default AppConnected;*/
+
+// export  default App
+
+
+
+// /*redux se usa atravez de container, los cantaines van a tener los estados de nuestra aplicacion,.
+// redux maneje el cambio de estado 
+// */
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,7 +5,10 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { store } from './store';
-ReactDOM.render(
+//EL store solo se llama una vez,
+ReactDOM.render (
 	<Provider store = {store} >
-	<App/></Provider>, document.getElementById('root'));
+	  <App/>
+	</Provider>, 
+	document.getElementById('root'));
 registerServiceWorker();
